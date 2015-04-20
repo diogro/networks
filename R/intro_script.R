19ed2a23697b1c61e94b6fc0878a2616544c1446
Diogo Melo <diogro@gmail.com>
2015-04-20 13:03:41 +0200

    basic plot

--- R/intro_script.R
+++ R/intro_script.R
@@ -1,6 +1,11 @@
 library('igraph')
 library('gdata')
 
-mat <- read.xls('~/Dropbox/Cursos/Participants Intro Network tools 2015/homo.skull.xls')
+#load('~/Dropbox/Cursos/Network tools 2015/2015 Course Materials/Hands on Computers/Praxis_workspace.RData')
+skull <- read.xls('~/Dropbox/Cursos/Network tools 2015/homo.skull.xls')
 
-
+graph <- graph.adjacency(as.matrix(skull), mode="undirected")
+c <- spinglass.community(graph)
+V(graph)$color <- c$membership
+graph$layout <- layout.fruchterman.reingold(graph)
+plot(graph)
